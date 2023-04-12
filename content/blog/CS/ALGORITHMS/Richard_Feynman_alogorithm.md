6b8ab80df38da5be3ebb96c6388ffd4446a7bcd0
zerozoo <zerozoo385@gmail.com>
2023-04-12 23:38:36 +0900

    add a post that explain feynman algorithm

---
+++ content/blog/CS/ALGORITHMS/Richard_Feynman_alogorithm.md
@@ -0,0 +1,10 @@
+---
+title: Richard_Feynman_alogorithm
+date: 2023-04-12 23:36:22
+---
+
+{% image "./images/feynmanAlgorithm.jpeg", "1. Writhe down the problem 2.  think very hard 3. Writhe down the answer" %}
+
+1. 칠판에 문제를 적는다.
+2. 골똘히 생각한다.
+3. 칠판에 답안을 적는다.
--- createPost.js
+++ createPost.js
@@ -6,7 +6,14 @@ function getKoreanDateTime() {
 	const month =
 		now.getMonth() < 9 ? "0" + (now.getMonth() + 1) : now.getMonth() + 1;
 	const date = now.getDate() < 10 ? "0" + now.getDate() : now.getDate();
-	return `${year}-${month}-${date}`;
+
+	const hours = now.getHours() < 10 ? "0" + now.getHours() : now.getHours();
+	const minutes =
+		now.getMinutes() < 10 ? "0" + now.getMinutes() : now.getMinutes();
+	const seconds =
+		now.getSeconds() < 10 ? "0" + now.getSeconds() : now.getSeconds();
+
+	return `${year}-${month}-${date} ${hours}:${minutes}:${seconds}`;
 }
 
 /**
